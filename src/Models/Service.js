9ae5ecfe0051e37080f4c158c0ce7cac2a1df24a
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-24 17:28:03 -0300

    service model creation

---
+++ src/Models/Service.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize)=>{
+    sequelize.define('Service', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {    // define currency (usd / ars)
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, { paranoid: true })
+}
