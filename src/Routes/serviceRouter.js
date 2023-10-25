e8ebab71a7e31a92d89f3a18157fc52bd8f05c78
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-25 14:47:11 -0300

    router get edit

--- src/Routes/serviceRouter.js
+++ src/Routes/serviceRouter.js
@@ -2,10 +2,12 @@ const Router = require('express');
 
 const URL = '../Handlers/serviceHandlers/'
 
+const getServiceRouter = require(URL + 'getServiceRouter');
 const postServiceHandler = require(URL + 'postServiceHandler');
 
 const serviceRouter = Router();
 
+serviceRouter.get('/', getServiceRouter);
 serviceRouter.post('/', postServiceHandler);
 
 module.exports = serviceRouter;
