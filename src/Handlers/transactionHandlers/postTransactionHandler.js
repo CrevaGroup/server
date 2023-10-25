637efeecfa03bdacb05a93dfe9396df0ace7d1a6
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-10-25 15:51:12 -0300

    creado controller/handler postTransaction y modelo

---
+++ src/Handlers/transactionHandlers/postTransactionHandler.js
@@ -0,0 +1,14 @@
+const postTransactionController = require("../../Controllers/transactionControllers/postTransactionController");
+
+const postTransactionHandler = async (req, res) => {
+    try {
+        const transactionInfo = req.body
+        const response = await postTransactionController(transactionInfo);
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(400).json(error.messge)
+        
+    }
+}
+
+module.exports = postTransactionHandler;
