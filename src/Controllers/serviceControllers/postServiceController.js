15c717452264209ac402cbc49f42ea468fa7b321
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-25 16:55:13 -0300

    post controller edit (title case)

--- src/Controllers/serviceControllers/postServiceController.js
+++ src/Controllers/serviceControllers/postServiceController.js
@@ -1,6 +1,11 @@
 const { Service } = require('../../db');
+const titleCase = require('../../Utils/titleCase');
 
 const postServiceController = async (serviceInfo) => {
+    serviceInfo = {
+        ...serviceInfo,
+        name: await titleCase(serviceInfo.name)
+    };
     const [service, created] = await Service.findOrCreate({ where: { name: serviceInfo.name }, defaults:{ ...serviceInfo } });
     if (created) return service;
     else throw new Error ('Ya existe un servicio con ese nombre.')
