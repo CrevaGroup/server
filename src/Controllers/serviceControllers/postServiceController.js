de08f39642877666f0c1885d158263a6d0a32cef
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-25 14:41:39 -0300

    controller fix

--- src/Controllers/serviceControllers/postServiceController.js
+++ src/Controllers/serviceControllers/postServiceController.js
@@ -1,7 +1,8 @@
 const { Service } = require('../../db');
 
 const postServiceController = async (serviceInfo) => {
-    const [service, created] = await Service.findOrCreate({ where: { name: serviceInfo.name }, default:{ ...serviceInfo } });
+    console.log(serviceInfo);
+    const [service, created] = await Service.findOrCreate({ where: { name: serviceInfo.name }, defaults:{ ...serviceInfo } });
     if (created) return service;
     else throw new Error ('Ya existe un servicio con ese nombre.')
 }
