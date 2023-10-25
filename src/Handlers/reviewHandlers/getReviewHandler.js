a5686768a6930956fa8b0ba0cc18a500ac9307b5
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-25 18:23:32 -0300

    Handler OK

---
+++ src/Handlers/reviewHandlers/getReviewHandler.js
@@ -0,0 +1,13 @@
+const getReviewController = require("../../Controllers/reviewControllers/getReviewController");
+
+const getReviewHandler = async(req, res) => {
+    try {
+        const {id} = req.query;
+        const response = await getReviewController(id);
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(404).json(error.message)
+    }
+}
+
+module.exports = getReviewHandler
