2cb87bbac0774108d19935d1d92dcae3840d311b
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-25 18:24:40 -0300

    Router OK

--- src/Routes/reviewRouter.js
+++ src/Routes/reviewRouter.js
@@ -3,9 +3,11 @@ const Router = require('express');
 const URL = '../Handlers/reviewHandlers/'
 
 const postReviewHandler = require(URL + 'postReviewHandler');
+const getReviewHandler = require(URL + 'getReviewHandler');
 
 const reviewRouter = Router();
 
 reviewRouter.post('/', postReviewHandler)
+reviewRouter.get('/', getReviewHandler)
 
 module.exports = reviewRouter
