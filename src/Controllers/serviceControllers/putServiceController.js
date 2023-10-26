5a8016a5d42f0fe448d50c85861c163d36ed37d1
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-26 08:50:16 -0300

    controller done

---
+++ src/Controllers/serviceControllers/putServiceController.js
@@ -0,0 +1,18 @@
+const { Service } = require('../../db');
+const titleCase = require('../../Utils/titleCase');
+
+const putServiceController = async (serviceInfo) => {
+    const service = await Service.findByPk(serviceInfo.id);
+    
+    if (!service) throw new Error ('No se encuentra el servicio solicitado.')
+
+    if (serviceInfo.name) service.name = serviceInfo.name;
+    if (serviceInfo.description) service.description = serviceInfo.description;
+    if (serviceInfo.price) service.price = serviceInfo.price;
+
+    await service.save();
+
+    return service;
+}
+
+module.exports = putServiceController;
