f14c43da23abed535069a4e8715899a33639511b
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-26 16:51:34 -0300

    emailbuilder fix

--- src/Utils/emailBuilder.js
+++ src/Utils/emailBuilder.js
@@ -1,5 +1,12 @@
-const emailBuilder = () => {
+const { SMTP_ADMINEMAIL } = process.env;
 
+const emailBuilder = (to, subject, message) => {
+    return {
+        from: 'group.creva@gmail.com',
+        to: to = 'ADMIN' ? SMTP_ADMINEMAIL : to,
+        subject: subject,
+        text: message
+    }
 }
 
 module.exports = emailBuilder;
