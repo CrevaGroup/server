a09471c1248d83de8b83481d3af596b4dfa063eb
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-26 11:32:03 -0300

    Controller OK

--- src/Controllers/reviewControllers/deleteReviewController.js
+++ src/Controllers/reviewControllers/deleteReviewController.js
@@ -2,7 +2,8 @@ const { Review } = require('../../db');
 
 const deleteReviewController = async (id) => {
     const review = await Review.destroy({where: {id: id}})
-    return review;
+    if(review) return 'Review eliminada correctamente'
+    throw new Error('No se encontro la review solicitada');
 }
 
 module.exports = deleteReviewController;
