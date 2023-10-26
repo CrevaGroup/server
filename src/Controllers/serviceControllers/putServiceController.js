dead428d8a764a83a3bfda57f108166f3b1f01b0
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-26 08:51:10 -0300

    adding titlecase fn

--- src/Controllers/serviceControllers/putServiceController.js
+++ src/Controllers/serviceControllers/putServiceController.js
@@ -6,7 +6,7 @@ const putServiceController = async (serviceInfo) => {
     
     if (!service) throw new Error ('No se encuentra el servicio solicitado.')
 
-    if (serviceInfo.name) service.name = serviceInfo.name;
+    if (serviceInfo.name) service.name = titleCase(serviceInfo.name);
     if (serviceInfo.description) service.description = serviceInfo.description;
     if (serviceInfo.price) service.price = serviceInfo.price;
 
