e1ddb730e4ca538d233085a8a48bcc8cf4dd38a4
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-26 10:38:55 -0300

    Controller fixed

--- src/Controllers/reviewControllers/putReviewController.js
+++ src/Controllers/reviewControllers/putReviewController.js
@@ -3,8 +3,9 @@ const { Review } = require('../../db');
 const putReviewController = async (reviewInfo) => {
     const review = await Review.findByPk(reviewInfo.id)
 
-    review.description = reviewInfo.description;
-    review.assessment = reviewInfo.assessment;
+    for(property in reviewInfo){
+        if(reviewInfo[property] && review.dataValues.hasOwnProperty(property)) review[property] = reviewInfo[property]
+    }
 
     await review.save()
 
