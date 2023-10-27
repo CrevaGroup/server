2946a6e0e4de9f2da78725b6533cd656f5390576
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-27 12:22:28 -0300

    handlers donde

--- src/Handlers/userHandlers/deleteUserHandler.js
+++ src/Handlers/userHandlers/deleteUserHandler.js
@@ -3,7 +3,7 @@ const deleteUserController = require('../../Controllers/userControllers/deleteUs
 const deleteUserHandler = async (req,res) => {
     try {
         const { id } = req.params;
-        const response = await deleteUserController(userInfo);
+        const response = await deleteUserController(id);
         res.status(200).json(response);
     } catch (err) {
         res.status(400).json(err.message);
