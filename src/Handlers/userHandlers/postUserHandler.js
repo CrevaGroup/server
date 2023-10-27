ab26a4c97f973a6ca2078ab0314216aba107d3ec
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-27 10:12:13 -0300

    handler setup

---
+++ src/Handlers/userHandlers/postUserHandler.js
@@ -0,0 +1,13 @@
+const postUserController = require('../../Controllers/userControllers/postUserController');
+
+const postUserHandler = async (req,res) => {
+    try {
+        const userInfo = req.body;
+        const response = postUserController(userInfo);
+        res.status(201).json(response);
+    } catch (err) {
+        res.status(400).json(err.message);
+    }
+}
+
+module.exports = postUserHandler;
