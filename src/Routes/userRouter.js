b32d92652115210f1da4271fbea51ffc5887ee20
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-27 10:10:19 -0300

    router setup

--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -2,7 +2,10 @@ const Router = require('express');
 
 const URL = '../Handlers/userHandlers/'
 
+const postUserHandler = URL + 'postUserHandler';
+
 const userRouter = Router();
 
+serviceRouter.post('/', postUserHandler);
 
 module.exports = userRouter
