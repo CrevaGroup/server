e514673c14d9241a700103a4f01d8f723f85a61c
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-28 17:54:35 -0300

    queryBuilder improvement + type filter

--- src/Utils/queryBuilder.js
+++ src/Utils/queryBuilder.js
@@ -1,14 +1,15 @@
-const { Op } = require("sequelize");
+const { Type } = require('../db');
+const { Op } = require('sequelize');
+const titleCase = require('../Utils/titleCase');
 
-const queryBuilder = (order, min, max, keyword) => {
+const queryBuilder = (order, min, max, type) => {
     let query = {};
-    if (min) query = { ...query, where: { price: { [Op.gte]: min } } };
-    if (max) query = { ...query, where: { price: { [Op.lte]: max } } };
     if (min && max) query = { ...query, where: { price: { [Op.between]: [min, max] } } };
-
-    // if (keyword) query = { ...query, where: { keyWord: { [Op.contains]: ['cv'] } } }
-    // if (keyword && min && max) query = { ...query, where: { ...query.where, keyWord: { [Op.contains]: ['cv'] } } }
-
+    else {
+        if (min) query = { ...query, where: { price: { [Op.gte]: min } } };
+        if (max) query = { ...query, where: { price: { [Op.lte]: max } } };
+    }
+    if (type) query = { ...query, include: [{ model: Type, where: { name: titleCase(type) } }] };
     query = { ...query, order: [[ 'price', order ? order : 'ASC' ]]};
     return query;
 }
