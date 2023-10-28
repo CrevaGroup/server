36cf4be61614a0836c416fbd587919f00d18da5f
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-28 19:58:36 -0300

    router fix

--- src/Routes/serviceRouter.js
+++ src/Routes/serviceRouter.js
@@ -6,7 +6,7 @@ const getServiceHandler = require(URL + 'getServiceHandler');
 const putServiceHandler = require(URL + 'putServiceHandler');
 const postServiceHandler = require(URL + 'postServiceHandler');
 const deleteServiceHandler = require(URL + 'deleteServiceHandler');
-const getSearchServiceHandler = require(URL + 'getSearchServiceHandler');
+const searchServiceHandler = require(URL + 'searchServiceHandler');
 
 const serviceRouter = Router();
 
@@ -14,6 +14,6 @@ serviceRouter.get('/', getServiceHandler);
 serviceRouter.put('/', putServiceHandler);
 serviceRouter.post('/', postServiceHandler);
 serviceRouter.delete('/:id', deleteServiceHandler);
-serviceRouter.post('/search', getSearchServiceHandler);
+serviceRouter.post('/search', searchServiceHandler);
 
 module.exports = serviceRouter;
