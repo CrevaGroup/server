663b714480bc774c26689467ef1586cbbc02a8ce
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-28 16:40:25 -0300

    get done

--- src/Controllers/typeControllers/getTypeController.js
+++ src/Controllers/typeControllers/getTypeController.js
@@ -1,19 +1,9 @@
-const { Service } = require('../../db');
-const { Op } = require("sequelize");
-const queryBuilder = require('../../Utils/queryBuilder');
+const { Type } = require('../../db');
 
-const getTypeController = async (id, order, min, max, keyword) => {
-    if (id) {
-        const service = await Service.findOne({ where: { id } });  
-        if (!service) throw new Error ('No se encuentra el servicio solicitado.')
-        return service;
-    }
-    else {
-        const query = queryBuilder(order, min, max, keyword);
-        const services = await Service.findAll({ ...query });
-        if (!services.length) throw new Error ('No se encuentran servicios.');
-        return services;
-    }
+const getTypeController = async () => {
+    const types = await Type.findAll();
+    if (!types.length) throw new Error ('No se encuentran tipos de servicios.');
+    return types;
 }
 
 module.exports = getTypeController;
