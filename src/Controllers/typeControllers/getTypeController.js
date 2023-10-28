01cf7feb523fc86081318a3cd6d7315d6e649a57
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-28 16:25:23 -0300

    controllers created

---
+++ src/Controllers/typeControllers/getTypeController.js
@@ -0,0 +1,19 @@
+const { Service } = require('../../db');
+const { Op } = require("sequelize");
+const queryBuilder = require('../../Utils/queryBuilder');
+
+const getTypeController = async (id, order, min, max, keyword) => {
+    if (id) {
+        const service = await Service.findOne({ where: { id } });  
+        if (!service) throw new Error ('No se encuentra el servicio solicitado.')
+        return service;
+    }
+    else {
+        const query = queryBuilder(order, min, max, keyword);
+        const services = await Service.findAll({ ...query });
+        if (!services.length) throw new Error ('No se encuentran servicios.');
+        return services;
+    }
+}
+
+module.exports = getTypeController;
---
+++ src/Controllers/typeControllers/postTypeController.js
@@ -0,0 +1,16 @@
+const { Service } = require('../../db');
+const titleCase = require('../../Utils/titleCase');
+const cloudinary = require('../../cloudinary')
+
+const postTypeController = async (serviceInfo) => {
+    serviceInfo = {
+        ...serviceInfo,
+        name: await titleCase(serviceInfo.name),
+        photo: (await cloudinary.uploader.upload(serviceInfo.photo)).secure_url
+    };
+    const [service, created] = await Service.findOrCreate({ where: { name: serviceInfo.name }, defaults:{ ...serviceInfo } });
+    if (created) return service;
+    else throw new Error ('Ya existe un servicio con ese nombre.')
+}
+
+module.exports = postTypeController;
