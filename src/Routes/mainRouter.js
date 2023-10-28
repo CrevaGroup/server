de68c3bf3bef93a2efa5b6377f6c102d4e9e64aa
braiaguirre <brianaguirre.sp94@gmail.com>
2023-10-28 16:37:27 -0300

    route done

--- src/Routes/mainRouter.js
+++ src/Routes/mainRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express');
 
 const userRouter = require('./userRouter');
+const typeRouter = require('./typeRouter');
 const reviewRouter = require('./reviewRouter');
 const serviceRouter = require('./serviceRouter');
 const transactionRouter = require('./transactionRouter');
@@ -8,6 +9,7 @@ const transactionRouter = require('./transactionRouter');
 const mainRouter = Router();
 
 mainRouter.use('/user', userRouter);
+mainRouter.use('/type', typeRouter);
 mainRouter.use('/review', reviewRouter);
 mainRouter.use('/service', serviceRouter);
 mainRouter.use('/transaction', transactionRouter);
