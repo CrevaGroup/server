aaaeb780a0b48d1ae79468a25252c8d7f9c143ab
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-28 17:13:15 -0300

    POST asociando la review a User y Service OK + GET con User y service asociados

--- src/Controllers/reviewControllers/postReviewController.js
+++ src/Controllers/reviewControllers/postReviewController.js
@@ -1,7 +1,8 @@
-const { Review } = require('../../db');
+const { Review, Service, User } = require('../../db');
 
 const postReviewController = async (reviewInfo) => {
     const review = await Review.create({...reviewInfo})
+
     return review;
 }
 
