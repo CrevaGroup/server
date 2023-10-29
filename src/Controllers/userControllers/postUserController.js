cbbbaac2196d021761b9dec71f4b44b78a52a019
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-29 15:11:09 -0300

    Login normal con photo OK

--- src/Controllers/userControllers/postUserController.js
+++ src/Controllers/userControllers/postUserController.js
@@ -1,17 +1,20 @@
 const { User } = require('../../db');
 const transporter = require('../../nodemailer');
 const titleCase = require('../../Utils/titleCase');
-const emailBuilder = require('../../Utils/emailBuilder')
+const emailBuilder = require('../../Utils/emailBuilder');
+const { cloudinary } = require('../../cloudinary');
+
 
 const postUserController = async (userInfo) => {
-    userInfo = {
-        ...userInfo,
-        fullName: titleCase(userInfo.fullName)
-    };
-    
     const [user, created] = await User.findOrCreate({ where: { id: userInfo.id }, defaults: { ...userInfo } });
 
     if (!created) throw new Error ('El email ya se encuentra registrado.');
+
+    userInfo = {
+        ...userInfo,
+        fullName: titleCase(userInfo.fullName),
+        photo: (await cloudinary.uploader.upload(userInfo.photo)).secure_url
+    };
 
     transporter.sendMail(emailBuilder(user.email, 'Bienvenid@', `Te damos la bienvenida a Creva, ${user.fullName}.`));
     
