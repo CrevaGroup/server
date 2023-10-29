a8f7ec7d023570079e06a462cdc1ef865a7507cd
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-29 12:51:15 -0300

    google login con photo OK (falta mejorar)

--- src/Controllers/userControllers/googleLoginController.js
+++ src/Controllers/userControllers/googleLoginController.js
@@ -2,19 +2,11 @@ const { User, Review, Transaction, Service } = require('../../db');
 const transporter = require('../../nodemailer');
 const emailBuilder = require('../../Utils/emailBuilder');
 const titleCase = require('../../Utils/titleCase');
+const cloudinary = require('../../cloudinary')
 
 const googleLoginController = async (userInfo) => {
-    userInfo = {
-        ...userInfo,
-        fullName: titleCase(userInfo.fullName)
-    };
-
-    const [user, created] = await User.findOrCreate({ where: { id: userInfo.id }, defaults: { ...userInfo } });
-    
-    if (created) transporter.sendMail(emailBuilder(user.email, 'Bienvenid@', `Te damos la bienvenida a Creva, ${user.fullName}.`));
-    
-    const newUser = await User.findOne({ 
-        where: {id: user.id}, 
+    const exist = await User.findOne({ 
+        where: {id: userInfo.id}, 
         include: [
         {
             model: Transaction,
@@ -29,8 +21,35 @@ const googleLoginController = async (userInfo) => {
         }] 
     })
 
-    if (!newUser.dataValues.age) return {
-        ...newUser.dataValues,
+    if(!exist){    
+
+        userInfo = {
+            ...userInfo,
+        fullName: titleCase(userInfo.fullName),
+        photo: (await cloudinary.uploader.upload(userInfo.photo)).secure_url
+    };
+
+    const [user, created] = await User.findOrCreate({ where: { id: userInfo.id }, defaults: { ...userInfo } });
+    
+    if (created) transporter.sendMail(emailBuilder(user.email, 'Bienvenid@', `Te damos la bienvenida a Creva, ${user.fullName}.`));
+    
+    const newUser = await User.findOne({ 
+        where: {id: user.id}, 
+        include: [
+            {
+                model: Transaction,
+                as: "buys",
+                include:[{
+                    model: Service
+                }]
+            },
+            {
+                model: Review,
+                as: "reviews"
+            }] 
+        })
+        if (!newUser.dataValues.age) return {
+            ...newUser.dataValues,
         access: true,
         complete: false
     }
@@ -39,6 +58,19 @@ const googleLoginController = async (userInfo) => {
         access: true,
         complete: true
     }
+    }
+
+    if (!exist.dataValues.age) return {
+        ...exist.dataValues,
+    access: true,
+    complete: false
+}
+return {
+    ...exist.dataValues,
+    access: true,
+    complete: true
+}
+        
 }
 
 module.exports = googleLoginController;
