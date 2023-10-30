958a20d3a64a79a493e02fd79d474ec8e69b9e30
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-29 23:08:15 -0300

    force en true

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const {conn} = require("./src/db")
 const PORT = process.env.PORT
 
 server.listen(PORT, () => {
-    conn.sync({ force: false }).then(()=>{console.log(`Connected to the DB: ${process.env.DB_NAME}`);})
+    conn.sync({ force: ture }).then(()=>{console.log(`Connected to the DB: ${process.env.DB_NAME}`);})
     console.log(`Server raised in port: ${PORT}`);
   });
