bac4256ed824d9e83947c4ebca817f1898aad0a4
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-30 11:45:18 -0300

    Filtros OK

--- src/Utils/queryBuilder.js
+++ src/Utils/queryBuilder.js
@@ -4,11 +4,9 @@ const titleCase = require('../Utils/titleCase');
 
 const queryBuilder = (order, min, max, type) => {
     let query = {};
+    // if (min) query = { ...query, where: { price: { [Op.gte]: min } } };
+    // if (max) query = { ...query, where: { price: { [Op.lte]: max } } };
     if (min && max) query = { ...query, where: { price: { [Op.between]: [min, max] } } };
-    else {
-        if (min) query = { ...query, where: { price: { [Op.gte]: min } } };
-        if (max) query = { ...query, where: { price: { [Op.lte]: max } } };
-    }
     if (type) query = { ...query, include: [{ model: Type, where: { name: titleCase(type) } }] };
     query = { ...query, order: [[ 'price', order ? order : 'ASC' ]]};
     return query;
