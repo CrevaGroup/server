17e5ec7a550abb912f871f60101c4c3fad8ba986
Martin Garcia Hervas <132405262+MartinGarciaHervas@users.noreply.github.com>
2023-10-31 16:44:48 -0300

    probemo

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const {conn} = require("./src/db.js")
 
 const PORT = process.env.PORT
 
-server.listen(PORT, () => {
+const startServer = async()=>{
+    try {
     conn.sync({ force: false }).then(()=>{console.log(`Connected to the DB: ${process.env.DB_NAME}`);})
+server.listen(PORT, () => {
     console.log(`Server raised in port: ${PORT}`);
   });
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+startServer()
