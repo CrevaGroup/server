3699c70ea3048d6de390623198fa1f9a61d0a197
Brian <52872480+braiaguirre@users.noreply.github.com>
2023-10-31 15:21:42 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE } = process.env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE, POSTGRES_URL_NON_POOLING } = process.env;
 
 //Models
 const userModel = require('./Models/User.js')
@@ -10,8 +10,11 @@ const serviceModel = require('./Models/Service.js')
 const transactionModel = require('./Models/Transaction.js')
 
 const sequelize = new Sequelize(
-    `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}?sslmode=require`,
+    `${POSTGRES_URL_NON_POOLING}?sslmode=require`,  
+    
     { logging: false, native: false });
+
+//`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}?sslmode=require`,
 
 userModel(sequelize);
 typeModel(sequelize);
