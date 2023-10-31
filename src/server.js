ef9047efc482d591edaf0644e47401dfe48f8240
Martin Garcia Hervas <132405262+MartinGarciaHervas@users.noreply.github.com>
2023-10-31 17:19:02 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const mainRouter = require("./Routes/mainRouter.js");
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors());
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+ });
 
 server.use(mainRouter);
 
