0165b52e81515672abcd67c9da3e475b93a12642
Brian <52872480+braiaguirre@users.noreply.github.com>
2023-10-31 15:14:09 -0300

    Update userRouter.js

--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -2,7 +2,7 @@ const Router = require('express');
 
 const URL = '../Handlers/userHandlers/';
 
-const getUserhandler = require(URL + 'getUserhandler.js');
+const getUserhandler = require(URL + 'getUserhandler');
 const putUserHandler = require(URL + 'putUserHandler.js');
 const postUserHandler = require(URL + 'postUserHandler.js');
 const deleteUserHandler = require(URL + 'deleteUserHandler.js');
