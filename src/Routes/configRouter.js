de2ee900e616daf6a1c7263a33ee01db71be1c13
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-01 17:48:53 -0300

    Route PUT OK

--- src/Routes/configRouter.js
+++ src/Routes/configRouter.js
@@ -2,11 +2,13 @@ const Router = require('express');
 
 const URL = '../Handlers/configHandlers/';
 
-const postConfigHandler= require(URL + 'postConfigHandler.js')
+const postConfigHandler = require(URL + 'postConfigHandler.js')
+const putConfigHandler = require(URL + 'putConfigHandler.js')
 
 const configRouter = Router();
 
 configRouter.post('/', postConfigHandler);
+configRouter.put('/', putConfigHandler);
 
 module.exports = configRouter
 
