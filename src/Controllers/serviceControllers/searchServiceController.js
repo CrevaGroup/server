3f5eefadb148f79284d0370cacd6aa9b13407fc6
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-01 18:02:17 -0300

    search controller v1

--- src/Controllers/serviceControllers/searchServiceController.js
+++ src/Controllers/serviceControllers/searchServiceController.js
@@ -1,10 +1,26 @@
-const { Service } = require('../../db');
+const { Service, Keyword, Type } = require('../../db');
+const titleCase = require('../../Utils/titleCase');
 const inputFinder = require('../../Utils/inputFinder');
 
 const searchServiceController = async (input) => {
-    const query = inputFinder(input);
-    const services = await Service.findAll({ ...query });
-    if (!services.length) return await Service.findAll();
+    input = titleCase(input).split(' ');
+    const keywords = await Keyword.findAll();
+    let services = [];
+    let servicesNames = [];
+    let types = [];
+    for (let keyword of keywords) 
+        if (input.includes(keyword.name)) {
+            const keywordDb = await Keyword.findOne({ where: {name: keyword.name }});
+            const keywordTypes = await keywordDb.getTypes();
+            types = [ ...types, ...keywordTypes ];
+        }
+    console.log(types);
+    for (let type of types) {
+        const typeDb = await Type.findOne({ where: {name: type.name }});
+        const typeServices = await typeDb.getServices();
+        services = [ ...services, ...typeServices ];
+    }
+
     return services;
 }
 
