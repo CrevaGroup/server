b62cf54a4a10df5df12c1d6166712cfe61c96ec6
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-01 16:26:37 -0300

    model fix

--- src/Models/Keyword.js
+++ src/Models/Keyword.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize)=>{
-    sequelize.define('Type', {
+    sequelize.define('Keyword', {
         id: {
             type: DataTypes.UUID,
             primaryKey: true,
