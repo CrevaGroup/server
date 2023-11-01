99400516e1a55a2ca6f3c082406130273ac2170f
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-01 18:29:48 -0300

    controller y handler put de IGpost

---
+++ src/Handlers/igPostHandlers/putIgPostHandler.js
@@ -0,0 +1,19 @@
+const putIgPostController = require("../../Controllers/igPostControllers/putIgPostController");
+
+
+const putIgPosHandler = async (req, res) =>{
+
+    try {
+        console.log("ingresa");
+        const post = req.body;
+        const response = await putIgPostController(post) 
+        res.status(201).json(response);
+
+    } catch (error) {
+        res.status(400).json(error.message);
+        
+    }
+
+}
+
+module.exports = putIgPosHandler
--- src/Routes/igPostRouter.js
+++ src/Routes/igPostRouter.js
@@ -4,11 +4,15 @@ const URL = '../Handlers/igPostHandlers/'
 
 const postIgPostHandler = require(URL + 'postIgPostHandler.js')
 const getIgPostHandler = require(URL + 'getIgPostHandler.js')
+const putIgPostHandler = require(URL + 'putIgPostHandler.js')
 
 
 const igPostRouter = Router()
 
 igPostRouter.post('/', postIgPostHandler);
 igPostRouter.get('/', getIgPostHandler);
+igPostRouter.put('/', putIgPostHandler);
+
+
 
 module.exports = igPostRouter;
