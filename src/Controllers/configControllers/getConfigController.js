ed0a4f9b12f33d5c8cb0679e8598c52859537327
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-01 17:54:59 -0300

    Route GET OK

---
+++ src/Controllers/configControllers/getConfigController.js
@@ -0,0 +1,11 @@
+const {Config} = require('../../db.js')
+
+const getConfigController = async () => {
+    const config = await Config.findAll();
+    if(config){
+        return config;
+    }
+    throw new Error('No existe una configuración')
+}
+
+module.exports = getConfigController
--- src/Routes/configRouter.js
+++ src/Routes/configRouter.js
@@ -4,11 +4,13 @@ const URL = '../Handlers/configHandlers/';
 
 const postConfigHandler = require(URL + 'postConfigHandler.js')
 const putConfigHandler = require(URL + 'putConfigHandler.js')
+const getConfigHandler = require(URL + 'getConfigHandler.js')
 
 const configRouter = Router();
 
 configRouter.post('/', postConfigHandler);
 configRouter.put('/', putConfigHandler);
+configRouter.get('/', getConfigHandler);
 
 module.exports = configRouter
 
