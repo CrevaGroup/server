3271ba7edaf40e8ede86bf167e09acf9c69e6c3c
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-01 17:06:20 -0300

    router edit

--- src/Routes/typeRouter.js
+++ src/Routes/typeRouter.js
@@ -3,12 +3,14 @@ const Router = require('express');
 const URL = '../Handlers/typeHandlers/';
 
 const getTypehandler = require(URL + 'getTypehandler');
+const putTypeHandler = require(URL + 'putTypeHandler');
 const postTypeHandler = require(URL + 'postTypeHandler');
 const deleteTypeHandler = require(URL + 'deleteTypeHandler');
 
 const typeRouter = Router();
 
 typeRouter.get('/', getTypehandler);
+typeRouter.put('/', putTypeHandler);
 typeRouter.post('/', postTypeHandler);
 typeRouter.delete('/:name', deleteTypeHandler);
 
