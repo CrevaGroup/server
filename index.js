3abbf35b8ae2da32fd32de137169a8a14e57d5fe
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-06 13:01:20 -0300

    prueba

--- index.js
+++ index.js
@@ -7,5 +7,3 @@ server.listen(PORT, () => {
     conn.sync({ force: true }).then(()=>{console.log(`Connected to the DB: ${process.env.DB_NAME}`);})
     console.log(`Server raised in port: ${PORT}`);
   });
-
-  //prueba
