0958d6dc3ffc2e1d09bcc2c07b9101bc77e1f42e
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-07 11:50:56 -0300

    prueba mp

--- src/Controllers/mercadopagoControllers/mercadopagoController.js
+++ src/Controllers/mercadopagoControllers/mercadopagoController.js
@@ -3,7 +3,7 @@ const { Transaction, Service } = require('../../db.js')
 const mercadopago = require('mercadopago');
 
 const mercadopagoController = async (paymentId) => {
-  // const payment = await mercadopago.payment.findById(Number(paymentId));
+  const payment = await mercadopago.payment.findById(Number(paymentId));
   // if (payment.body.status === "approved") {
 
   //   const userId = payment.body.additional_info.payer.address.zip_code
