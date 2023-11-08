824b43111c3648934474d4aa6cc9c9549bea8193
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-08 17:07:05 -0300

    elimine un console log

--- src/Controllers/configControllers/getConfigController.js
+++ src/Controllers/configControllers/getConfigController.js
@@ -4,7 +4,6 @@ const {Config} = require('../../db.js')
 const getConfigController = async () => {
 
     const {data} = await axios.get('https://ipinfo.io')
-    console.log(data.ip)
     const country = {country: data.country}
     const config = await Config.findOne();
    
