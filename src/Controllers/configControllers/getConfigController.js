ff0bca8ae7099942abb76ad7754c4453be15f8df
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-08 16:56:31 -0300

    agregue en el controller getConfigController para devolver Pais por ip

--- src/Controllers/configControllers/getConfigController.js
+++ src/Controllers/configControllers/getConfigController.js
@@ -1,9 +1,15 @@
+const  axios  = require('axios');
 const {Config} = require('../../db.js')
 
 const getConfigController = async () => {
+
+    const {data} = await axios.get('https://ipinfo.io')
+    console.log(data.ip)
+    const country = {country: data.country}
     const config = await Config.findOne();
-    if(config){
-        return config;
+   
+    if(config && country){
+        return {...config, ...country};
     }
     throw new Error('No existe una configuración')
 }
