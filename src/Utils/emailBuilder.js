09e82f51d4a739b4a24e801fb75526cbda529f7e
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-09 14:35:05 -0300

    Plantillas listas

--- src/Utils/emailBuilder.js
+++ src/Utils/emailBuilder.js
@@ -1,11 +1,39 @@
 const { SMTP_ADMINEMAIL } = process.env;
 
-const emailBuilder = (to, subject, message) => {
+const emailBuilder = (to, subject, message, messageHtml) => {
     return {
         from: 'group.creva@gmail.com',
         to: to === 'ADMIN' ? SMTP_ADMINEMAIL : to,
         subject: subject,
-        text: message
+        text: message,
+        html: messageHtml,
+        attachments:[
+            {
+                filename: "Gmail.svg",
+                path: "./PlantillasEmail/Fotitos/BxBxlGmail.svg",
+                cid: "Gmail"
+            },
+            {
+                filename: "Creva.svg",
+                path: "./PlantillasEmail/Fotitos/logo.png",
+                cid: "creva"
+            },
+            {
+                filename: "wapp.svg",
+                path: "./PlantillasEmail/Fotitos/MdiWhatsapp.svg",
+                cid: "Wapp"
+            },
+            {
+                filename: "Ig.svg",
+                path: "./PlantillasEmail/Fotitos/PhInstagramLogo.svg",
+                cid: "Ig"
+            },
+            {
+                filename: "Linkedin.svg",
+                path: "./PlantillasEmail/Fotitos/UilLinkedin.svg",
+                cid: "Linkedin"
+            }
+        ]
     }
 }
 
