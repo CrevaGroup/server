310b0675449bdde8bdb54f29e818dd7d1faff2da
Martin Garcia Hervas <132405262+MartinGarciaHervas@users.noreply.github.com>
2023-11-14 12:12:56 -0300

    Update getConfigController.js

--- src/Controllers/configControllers/getConfigController.js
+++ src/Controllers/configControllers/getConfigController.js
@@ -6,13 +6,13 @@ const getConfigController = async (ip) => {
     const date = new Date()
 
     const config = await Config.findOne();
-
+console.log('hola')
     if(!config){
         await dollarUpdate()
     } else if (date.toDateString() !== config.dataValues.updatedAt.toDateString()){
         await dollarUpdate()
     }
-
+console.log('chau')
     const config2 = await Config.findOne();
 
     const {data} = await axios.get(`https://ipinfo.io/${ip}`)
