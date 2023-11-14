a0b0f253d73822dce4736f620921aed95115929b
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-13 21:22:34 -0300

    Chau vercel json + server

--- src/server.js
+++ src/server.js
@@ -1,29 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const mainRouter = require("./Routes/mainRouter.js");
-const dolarUpdate = require("./Utils/dollarUpdate.js");
+const mainRouter = require("./Routes/mainRouter");
+const dolarUpdate = require("./Utils/dollarUpdate");
+
 
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-    next();
- });
-
+server.use(cors());
 server.use(mainRouter);
-dolarUpdate() 
+//descomentar esta linea para que empiece a funcionar la actualizacion automatica del dolar
+//dolarUpdate() 
 
 
 module.exports = server
