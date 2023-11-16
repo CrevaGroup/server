839a015898b9639343e6bc90da0ec5ff5f05e4de
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-16 12:46:48 -0300

    put user ok

--- src/Controllers/userControllers/putUserController.js
+++ src/Controllers/userControllers/putUserController.js
@@ -1,7 +1,18 @@
 const { User } = require('../../db.js');
 
 const putUserController = async (userInfo) => {
-    const user = await User.findOne({ where: { id: userInfo.id } });
+    const user = await User.findOne({ where: { id: userInfo.id }, include: [
+        {
+            model: Transaction,
+            as: "buys",
+            include:[{
+                model: Service
+            }]
+        },
+        {
+            model: Review,
+            as: "reviews"
+        }] });
     if (!user) throw new Error ('No se encontró el usuario.');
 
     for (property in userInfo) {
