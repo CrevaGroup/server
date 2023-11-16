4be806b25671f438be04ea88e3f5de3c844e5d0c
Martin Garcia Hervas <punighervas@gmail.com>
2023-11-16 13:01:42 -0300

    Backup demo final

--- src/db.js
+++ src/db.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { pg } = require('pg');
-const { POSTGRES_URL_NON_POOLING } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 //Models
-const userModel = require('./Models/User.js')
-const typeModel = require('./Models/Type.js')
-const reviewModel = require('./Models/Review.js')
-const serviceModel = require('./Models/Service.js')
-const transactionModel = require('./Models/Transaction.js')
+const userModel = require('./Models/User')
+const typeModel = require('./Models/Type')
+const reviewModel = require('./Models/Review')
+const serviceModel = require('./Models/Service')
+const transactionModel = require('./Models/Transaction')
 const igPostModel = require('./Models/IGpost.js')
 const configModel = require('./Models/Config.js')
-const textBlogModel = require('./Models/textBlog.js')
+const textBlogModel = require('./Models/TextBlog.js')
 const keywordModel = require('./Models/Keyword.js')
 
 const sequelize = new Sequelize(
-    `${POSTGRES_URL_NON_POOLING}?sslmode=require`,  
-    { logging: false, native: false, dialect: "postgres", dialectModule: pg });
-
-//`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DATABASE}?sslmode=require`,
+    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    { logging: false, native: false });
 
 userModel(sequelize);
 typeModel(sequelize);
